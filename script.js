c9f10fdde377f98d5f0b2847f189f82dee810eab
joaosilvacarvalho17-sys <joao.silva.carvalho17@escola.pr.gov.br>
2026-04-30 10:19:06 -0300

    css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function salvarCarro(event) {
-    
     event.preventDefault();
+
     let titulo = document.getElementById('title').value;
     let preco = document.getElementById('preco').value;
     let marca = document.getElementById('marca').value;
-    let modelo = document.getElementById('modelo').value; 
+    let modelo = document.getElementById('modelo').value;
 
-    let cambioSelecionado = document.querySelector('input [name="marcha"]:checked');
-    
+    let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
+
     let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
     let carro = {
@@ -15,36 +15,36 @@ function salvarCarro(event) {
         preco,
         marca,
         modelo,
-        cambio,
+        cambio
     };
 
     let carros = JSON.parse(localStorage.getItem("carros")) || [];
     carros.push(carro);
-    localStorage.setItem("caros", JSON.stringify(carros));
+    localStorage.setItem("carros", JSON.stringify(carros));
 
-    adcionarNaTela(carro);
+    adicionarNaTela(carro);
 
     document.querySelector("form").reset();
 }
 
 function adicionarNaTela(carro) {
-    let lista = document.getElementById('listaCarros')
-    let card = document.createElement('div')
+    let lista = document.getElementById('listaCarros');
+    let card = document.createElement('div');
     card.classList.add('card');
 
     card.innerHTML = `
     <h3>${carro.titulo}</h3>
     <p><strong>Preço:</strong> R$ ${carro.preco}</p>
-    <p><strong>Marca:</strong> R$ ${carro.marca}</p>
-    <p><strong>Modelo:</strong> R$ ${carro.modelo}</p>
-    <p><strong>Cambio:</strong> R$ ${carro.cambio}</p>
+    <p><strong>Marca:</strong>  ${carro.marca}</p>
+    <p><strong>Modelo:</strong>  ${carro.modelo}</p>
+    <p><strong>Câmbio:</strong> ${carro.cambio}</p>
     `
 
-        lista.appendChild(card)
+    lista.appendChild(card);
 }
 
-window.pmlpad = function () {
-    let carros = JSON.parse(localStorage.getItem('carros'))|| [];
+window.onload = function () {
+    let carros = JSON.parse(localStorage.getItem('carros')) || [];
 
     carros.forEach(carro => {
         adicionarNaTela(carro);
